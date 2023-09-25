a9c725958d33acbd1ce876206be1baeaa0c03ada
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-09-25 21:15:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,10 +41,9 @@ class evamaria(Client):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-
+API_ID = int(os.environ.get('API_ID', ''))
+API_HASH = os.environ.get('API_HASH', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
